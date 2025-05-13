d8b0cf393d833c4e7b222a1fa669125d16835c7d
aadarsh <aadarsh153@gmail.com>
2025-05-13 22:45:17 +0530

    Shell-Scripting

---
+++ 02.Shebang.sh
@@ -0,0 +1,4 @@
+echo "testing the script without shebang"
+
+
+#!/bin/bash-->"shebang" refers to the first line of a Bash script that specifies which interpreter will be used when executing the script.
---
+++ 03.variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Mike: Hi john, How are you?"
+echo "John: Hello mike, I am doing good! How are you?"
+echo "Mike: I am fine john"
+
