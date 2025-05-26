bead0c70735c12f1cc770f1c081cb1e405c82cff
aadarsh <aadarsh153@gmail.com>
2025-05-26 21:20:16 +0530

    Shell-Script

--- 24.word-count.sh
+++ 24.word-count.sh
@@ -8,11 +8,11 @@ then
 else
     echo "make sure $FILE exits"
 fi
-
+FILES=$(find $FILE)
 while IFS= read -r line
 do
 RESU=$(echo $line | cut -d " " -f1 | sort | uniq -d)
-done <<< $FILE
+done <<< $FILES
 
 
 echo "this is :$RESU"
