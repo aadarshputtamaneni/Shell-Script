77aaa601770cbe8520c1ca6be48eb667ac30a50f
aadarsh <aadarsh153@gmail.com>
2025-05-26 21:05:23 +0530

    Shell-Script

--- 24.word-count.sh
+++ 24.word-count.sh
@@ -2,13 +2,9 @@
 FILE=/tmp/sample.txt
 
 if [ -f $FILE ]
-then
+then 
     echo "already $FILE exits"
 else
     echo "make sure $FILE exits"
 fi
 
-while IFS= read -r line
-do
-RESULT=$( echo $line | cut -d" " -f1 | sort | uniq -d)
-done <<< $FILE
