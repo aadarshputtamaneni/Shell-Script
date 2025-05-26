e610793477739bfa3297ac2d6e24fc427fda1474
aadarsh <aadarsh153@gmail.com>
2025-05-26 20:20:06 +0530

    Shell-Script

--- 23.checkvowels.sh
+++ 23.checkvowels.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-read -p "enter string:" input
+read -p "enter string:" 
 vowel_count=$(echo "$input" | grep -o -i "[aeiou]" | wc -l)
 
 echo "number of vowels in give string:$vowel_count"
