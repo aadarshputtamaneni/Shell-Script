42aff2a6d51084cfd9182ef4b1a04302a31170f7
aadarsh <aadarsh153@gmail.com>
2025-05-29 02:06:52 +0530

    Shell-Script

--- 25.service-monitoring.sh
+++ 25.service-monitoring.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 SERVICE_NAME="nginx"
 
-if [ "$(systemctl is-active $SERVICE_NAME)" != "active" ]
+if [ "$(systemctl is-active $SERVICE_NAME)" = "active" ]
 then
     echo "$SERVICE_NAME is not active,restaring"
+    exit 1
     systemctl restart $SERVICE_NAME 
 else
     echo "$SERVICE_NAME is not active"
