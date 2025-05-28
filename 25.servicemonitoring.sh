f064bb0e2b85e926e9890f978b5bc2e72b931cb1
aadarsh <aadarsh153@gmail.com>
2025-05-29 00:43:12 +0530

    Shell-Script

--- 25.servicemonitoring.sh
+++ 25.servicemonitoring.sh
@@ -4,10 +4,9 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-if [ -z $SERVICE_NAME ]
+if [ -z $SERVICE_NAME -ne "active" ]
 then
     echo -e "$R $SERVICE_NAME is not active $N"
-    yum install nginx -y
     systemctl start $SERVICE_NAME
 else
     echo -e "$G $SERVICE_NAME is active $N"
