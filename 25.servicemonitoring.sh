fe70d7db35db16b8df117038c00399ab3574e5a6
aadarsh <aadarsh153@gmail.com>
2025-05-29 00:56:00 +0530

    Shell-Script

--- 25.servicemonitoring.sh
+++ 25.servicemonitoring.sh
@@ -7,8 +7,8 @@ N="\e[0m"
 if [ "$SERVICE_NAME" != "active" ]
 then
     echo -e "$R $SERVICE_NAME is not active $N"
-    exit 1
 else
-    systemctl restart $SERVICE_NAME
     echo -e "$G $SERVICE_NAME is active $N"
 fi
+
+systemctl restart $SERVICE_NAME
