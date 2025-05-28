43341d85969eb1d9b02098f853aa2e5f4c2906cd
aadarsh <aadarsh153@gmail.com>
2025-05-28 23:15:50 +0530

    Shell-Script

--- 23.checkvowels.sh
+++ 23.checkvowels.sh
@@ -2,5 +2,6 @@
 echo "Enter your string:"
 read COUNT
 vowel_count=$( echo $COUNT | grep -i -o "[aeiou]" | wc -l)
-
+# -i: This option makes the grep command case-insensitive.
+# -o: Display only the matched pattern, not the whole line:
 echo "number of vowels in given string:$vowel_count"
---
+++ 24.backup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+BACKUP_SOURCE="/tmp/"
+BACKUP_DESTINATION="/backups"
+BACKUP_DATE=$(date +%Y+%m+%d+%H+%M+%S)
+BACKUP_FILENAME="backup_$BACKUP_DATE.tar.gz"
+mkdir -p "$BACKUP_DESTINATION/$BACKUP_DATE"
+
+tar -czf "$BACKUP_DESTINATION/$BACKUP_DATE/$KUP_FILENAME" "$BACKUP_SOURCE"
+
+if [ $? -eq 0 ]
+then
+    echo "backup successful:$BACKUP_FILENAME"
+else
+    echo "backup failed"
+fi
+
+
+
+
+: '
+"tar [options] [archive-file] [file or directory to be archived]
+
+tar -cvf archive_name.tar /path/to/directory_or_file
+
+Create (-c). Create a new archive.
+Extract (-x). Extract files from an archive.
+Verbose (-v). Show the detailed output of the process.
+File (-f). Specify the archive file.
+Compress with gzip (-z). The archive will be compressed using gzip, resulting in a .tar.gz file.
+Compress with bzip2 (-j). Compress the archive using bzip2, resulting in a .tar.bz2 file.
+Compress with xz (-J). Compress the archive using xz, resulting in a .tar.xz file."
+ '
