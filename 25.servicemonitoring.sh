f3b75c9aca91df0f9435a24a8899aff5a2591ae6
aadarsh <aadarsh153@gmail.com>
2025-05-29 00:45:10 +0530

    Shell-Script

--- 25.servicemonitoring.sh
+++ 25.servicemonitoring.sh
@@ -4,7 +4,7 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-if [ -z $SERVICE_NAME - ne "active" ]
+if [ "$SERVICE_NAME" != "active" ]
 then
     echo -e "$R $SERVICE_NAME is not active $N"
     systemctl start $SERVICE_NAME
