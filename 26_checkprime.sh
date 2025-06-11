e7340531aacc112d80de09cf537fbe34dcfccd7a
aadarsh <aadarsh153@gmail.com>
2025-06-11 18:33:33 +0530

    Shell_Script

--- 26_checkprime.sh
+++ 26_checkprime.sh
@@ -7,16 +7,16 @@ if [ $SOURCE -le 1 ]
 then
     echo "your number is $SOURCE not prime"
     exit 1
-else
-    echo "your number is $SOURCE is prime"
-fi
+ fi
  # Start checking from 2 to number-1
 for (( i=2; i<$SOURCE; i++ ))
 do
     # If number is divisible by i, it's not prime
-    if (( SOURCE % i == 0 ));
-    then
+    if [ $SOURCE % i == 0 ]
+    then 
         echo "$SOURCE is not a prime number"
         exit 1
+    else
+        echo "$SOURCE is prime"
     fi
 done
