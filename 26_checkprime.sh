96d736ff307b35e0ae3dbbc9487c693a3ae50898
aadarsh <aadarsh153@gmail.com>
2025-06-11 18:54:39 +0530

    Shell_Script

--- 26_checkprime.sh
+++ 26_checkprime.sh
@@ -7,19 +7,18 @@ read num
 # Check if the number is less than or equal to 1
 if [ $num -le 1 ]; then
   echo "$num is not a prime number"
-  exit 1
+  exit
 fi
 
 # Loop from 2 to num-1
 for (( i=2; i<$num; i++ ))
 do
   # If num is divisible by i, it's not prime
-  if (( num % i == 0 ))
-  then
-      echo "$num is not a prime number"
-      exit 1
-  else
-      echo "$num is a prime number"
+  if (( num % i == 0 )); then
+    echo "$num is not a prime number"
+    exit
   fi
 done
- 
+
+# If no divisors found, it's a prime number
+echo "$num is a prime number"
